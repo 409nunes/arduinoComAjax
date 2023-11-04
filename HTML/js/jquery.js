844be59576e5a5dea912e8d91a41ad41ae4fd179
eduardo <eduardosky2011@hotmail.com>
2023-11-04 14:50:59 -0300

    acrescentando

--- HTML/js/jquery.js
+++ HTML/js/jquery.js
@@ -10,7 +10,7 @@
  * http://jquery.org/license
  *
  * Date: Thu Sep 20 2012 21:13:05 GMT-0400 (Eastern Daylight Time)
- */
+ * apagar/
 (function (window, undefined) {
   var // A central reference to the root jQuery(document)
     rootjQuery,
